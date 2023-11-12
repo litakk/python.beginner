86f76841c3f1efefbeed4059127789bdc663e5b8
litakk <140828663+litakk@users.noreply.github.com>
2023-11-12 20:51:32 +0300

    Update12.11.2023

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,19 +1,34 @@
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def speak(self):
+#         raise NotImplementedError("Subclass must implement abstract method")
+
+# class Dog(Animal):
+#     def speak(self):
+#         return self.name+' says Woof!'
+
+# class Cat(Animal):
+#     def speak(self):
+#         return self.name+' says Meow!'
+
+# dog = Dog("Max")
+# print(dog.speak())
+# cat = Cat("Alice")
+# print(cat.speak())
+
+
+
 class Animal:
     def __init__(self, name):
         self.name = name
 
     def speak(self):
-        raise NotImplementedError("Subclass must implement abstract method")
+     return NotImplementedError('Subbclass must implement abstract method')
 
 class Dog(Animal):
     def speak(self):
-        return self.name+' says Woof!'
-
-class Cat(Animal):
-    def speak(self):
-        return self.name+' says Meow!'
-
-dog = Dog("Max")
+     return self.name+' says Woof'
+dog = Dog('My Dog')
 print(dog.speak())
-cat = Cat("Alice")
-print(cat.speak())
