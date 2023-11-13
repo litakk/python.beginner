dbe25a8c080caeccc59ad192c18950cc7f8f02c2
litakk <140828663+litakk@users.noreply.github.com>
2023-11-13 22:48:52 +0300

    Update13.11.2023

--- Decorator.py
+++ Decorator.py
@@ -17,15 +17,14 @@
 # fn_3(fn_1)
 # fn_3(fn_2)
 
-
-
-
 lesson  = "RU: Декораторы и обертки. EN: Decorators & Wrappers"
 # Decorators — это функции, которые принимают в качестве аргумента другую функцию, добавляют какую-то функциональность,
 # и затем возвращаем другую функцию. И все это без изменения исходного кода оригинала.
 # функция, которую мы передали. В Python функции являются объектами первого класса, а это значит, что мы можем
 # передать их в качестве аргументов другим функциям. Мы также можем вернуть их как значения из других функций.
 # Это основа декораторов.
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 # Синтаксис - SYNTAX
 
@@ -42,7 +41,7 @@ lesson  = "RU: Декораторы и обертки. EN: Decorators & Wrappers
 # display() # == decorator_function(display)()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
+# БАЗОВЫЙ ДЕКОРАТОР
 # def decorator_function(original_function):
 #     def wrapper_function(*args, **kwargs):
 #         print(f"Wrapper executed this before {original_function.__name__}")
@@ -56,26 +55,89 @@ lesson  = "RU: Декораторы и обертки. EN: Decorators & Wrappers
 # x = [1, 2, 3, 4, 5]
 # original_function(x)
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# определение декоратора
+# def декоротивная_функция(func):
+#     def покрывала_функции():
+#         print("Это до выполнения функции")
+#         result = func()  # RU: вызов оригинальной функции
+#         print("Это после выполнения функции")
+#         return result
+#     return покрывала_функции
+
+# Создание оригинальной функции
+# @декоротивная_функция
+# def оригинальная_функция():
+#     print("Это призвано быть оригинальной функцией")
+
+# функция_для_использования = декоротивная_функция(оригинальная_функция)
+
+# Призив оригинальной функции
+# оригинальная_функция()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Практический пример 1 - узнать время выполнения функции с помощью декоратора.
+# декоратор для расчета продолжительности
+# принимается любой функцией.
+
+# def calculate_time(func):
+    # added arguments inside the inner1,
+    # if function takes any arguments,
+    # can be added like this.
+    # def inner1(*args, **kwargs):
+    #     # storing time before function execution
+    #     begin = time.time()
+    #     func(*args, **kwargs)
+    #     # storing time after function execution
+    #     end = time.time()
+    #     print("Total time taken in : ", func.__name__, end - begin)
+    # return inner1
+
+# это можно добавить к любой существующей функции,
+# в данном случае для вычисления факториала
+
+# @calculate_time
+# def factorial(num):
+# спать 1 секунду, потому что это занимает гораздо меньше времени
+# чтобы вы могли увидеть реальную разницу
+    # time.sleep(1)
+    # print(math.factorial(num))
+
+# вызов функции.
+# factorial(10)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# ВОЗВРАЩАЕМ ЗНАЧЕНИЕ
 
 
+# def hello_decorator(func):
+    # def inner1(*args, **kwargs):
+    #     print("before Execution")
+# получение возвращаемого значения
+        # returned_value = func(*args, **kwargs)
+        # print("after Execution")
+# возвращаем значение в исходный кадр
+    #     return returned_value
+    # return inner1
 
+#добавляем декоратор в функцию
+# @hello_decorator
+# def sum_two_numbers(a, b):
+#     print("Inside the function")
+#     return a + b
 
+# a, b = 1, 2
 
+# получение значения через возврат функции
+# sum = sum_two_numbers(a, b)
+# print("Sum =", sum)
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# ПРОСТО ТАК
 
 # def decorator_glavniy(argument_function):
 #     def wrapper_func(*args, **kwargs):
@@ -91,6 +153,7 @@ lesson  = "RU: Декораторы и обертки. EN: Decorators & Wrappers
 # argument_function(x)
 
 #  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 # import time
 # import math
 # def calculate_time(func):
