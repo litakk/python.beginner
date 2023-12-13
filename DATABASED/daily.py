51a63f34e49d80d53ba076ca3ca0f6a86a0aecce
litakk <140828663+litakk@users.noreply.github.com>
2023-12-13 19:55:51 +0500

    Update in DB - DataBased

--- DATABASED/daily.py
+++ DATABASED/daily.py
@@ -397,3 +397,186 @@
 
 # ALTER TABLE customers ADD COLUMN full_name VARCHAR(255);
 # UPDATE customers SET full_name = CONCAT(name, ', email: ', email);
+
+# =================================================================================
+# =================================================================================
+# =================================================================================
+
+# УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 
+# УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 
+# УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 УРОК 4 
+
+# PYTHON + PSQL
+# CREATE INDEX index_name
+# ON table_name (column1, column2, ...);
+
+# CREATE INDEX users_names_inx
+# ON users (name) WHERE name IS 'Alex%';
+
+# Делая это, мы сообщаем базе данных создать индекс для таблицы table_name, 
+# и этот индекс будет основан на column1, column2, ... columns.
+
+
+# =================================================================================
+# =================================================================================
+
+
+# Для работы с postgresql нам необходимо установить psycopg2-binary
+# pip install psycopg2-binary
+import os
+import faker
+import psycopg2
+import requests
+
+# Connecting to the database
+conn = psycopg2.connect(
+    user="postgres",
+    password="qweqweqwe",
+    host="localhost",
+    port="5432",
+    database="postgres"
+)
+# This is for autocommiting the changes
+# It means that we don't need to commit every time
+# to save the changes in the database
+# Otherwise, we need to commit every time when we
+# change something by using conn.commit()
+conn.autocommit = True
+
+# This is the cursor
+cur = conn.cursor()
+
+# Creating the table
+cur.execute('''
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    birthdate DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    bio TEXT
+);
+''')
+cur.execute('''INSERT INTO table_name 
+    VALUES (...)
+''')  # - to insert data into the table
+
+# ========================================================
+# Insert 10000 rows of people into users table
+# Each should have name, email, password, country
+# Use faker to insert all the data
+fake = faker.Faker()
+for i in range(10):
+    cur.execute('''
+        INSERT INTO users 
+        (birthdate, first_name, last_name, email, bio) 
+        VALUES (%s, %s, %s, %s, %s);
+        ''', (
+        fake.date(),       # - generates 'YYYY-MM-DD'
+        fake.first_name(), # - generates first name
+        fake.last_name(),
+        fake.email(),
+        fake.text()
+    ))
+# ========================================================
+# To write a code "pg_dump -U postgres -h localhost -p 5432 -F c -f база.psql postgres"
+# to create a backup of the database with the help of python
+def save_db_at_this_point():
+    os.system('pg_dump -U postgres -h localhost -p 5432 -F c -f db.psql postgres')
+# ========================================================
+# To print the table in the terminal
+cur.execute('''SELECT * FROM users;''')
+rows = cur.fetchone()
+
+if rows:
+    print(rows)
+else:
+    print("No rows returned.")
+# ========================================================
+# .com - commercial
+# .org - organization
+# .net - network
+# .gov - government
+# .ru  - Russia
+# .ua  - Ukraine
+# .uz  - Uzbekistan
+# .us  - United States# To work with postgresql we need to install psycopg2-binary
+# pip install psycopg2-binary
+import os
+import faker
+import psycopg2
+import requests
+
+# Connecting to the database
+conn = psycopg2.connect(
+    user="postgres",
+    password="qweqweqwe",
+    host="localhost",
+    port="5432",
+    database="postgres"
+)
+# This is for autocommiting the changes
+# It means that we don't need to commit every time
+# to save the changes in the database
+# Otherwise, we need to commit every time when we
+# change something by using conn.commit()
+conn.autocommit = True
+
+# This is the cursor
+cur = conn.cursor()
+
+# Creating the table
+cur.execute('''
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    birthdate DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    bio TEXT
+);
+''')
+cur.execute('''INSERT INTO table_name 
+    VALUES (...)
+''')  # - to insert data into the table
+
+# ========================================================
+# Insert 10000 rows of people into users table
+# Each should have name, email, password, country
+# Use faker to insert all the data
+fake = faker.Faker()
+for i in range(10):
+    cur.execute('''
+        INSERT INTO users 
+        (birthdate, first_name, last_name, email, bio) 
+        VALUES (%s, %s, %s, %s, %s);
+        ''', (
+        fake.date(),       # - generates 'YYYY-MM-DD'
+        fake.first_name(), # - generates first name
+        fake.last_name(),
+        fake.email(),
+        fake.text()
+    ))
+# ========================================================
+# To write a code "pg_dump -U postgres -h localhost -p 5432 -F c -f база.psql postgres"
+# to create a backup of the database with the help of python
+def save_db_at_this_point():
+    os.system('pg_dump -U postgres -h localhost -p 5432 -F c -f db.psql postgres')
+# ========================================================
+# To print the table in the terminal
+cur.execute('''SELECT * FROM users;''')
+rows = cur.fetchone()
+
+if rows:
+    print(rows)
+else:
+    print("No rows returned.")
+# ========================================================
+# .com - commercial
+# .org - organization
+# .net - network
+# .gov - government
+# .ru  - Russia
+# .ua  - Ukraine
+# .uz  - Uzbekistan
+# .us  - United States
